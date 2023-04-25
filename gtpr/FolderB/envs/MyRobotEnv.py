eccf1921a72b9ca515063c0ce5fd8b4c0c5a6e91
mraRTee <tomderoza@gmail.com>
2023-04-25 20:55:06 +0200

    Added SINUS simulation part and some refactor

--- gtpr/FolderB/envs/MyRobotEnv.py
+++ gtpr/FolderB/envs/MyRobotEnv.py
@@ -1,85 +1,17 @@
 import numpy as np
 from gym import utils
 from gym.envs.mujoco import mujoco_env
-import mujoco_py
 import os
 import time
 import mujoco_py as mjp
-
-
-
 from dm_control import suite,mujoco
-
-
-# cnter=0
-
-# A1_qpos = 7
-# A2_qpos = 17
-# A3_qpos = 28
-
-# # Ez nem tudom mennyire igaz
-# A1_qvel = 6
-# A2_qvel = 16
-# A3_qvel = 27
-# class Static():
-#     cnt1=0
-#     cnt2=0
-#     x=4
-#     y=4
-# class EnvClassName(mujoco_env.MujocoEnv, utils.EzPickle):
-#     def __init__(self):
-    
-#         utils.EzPickle.__init__(self)
-#         FILE_PATH = '' # Absolute path to your .xml MuJoCo scene file 
-#                         # OR.
-#         FILE_PATH = os.path.join(os.path.dirname(__file__), "compiled.xml")
-#         frame_skip = 2
-#         #mujoco_env.MujocoEnv.__init__(self, FILE_PATH, frame_skip)
-        
-#         model = mjp.load_model_from_path("/home/rot1bp/Desktop/work_custom_mujoco_project/OE/gtpr/FolderB/envs/compiled.xml")
-#         sim = mjp.MjSim(model)
-#         # másik mujoco az adatok kinyerésére
-#         #model = mujoco.MjModel.from_xml_path(FILE_PATH)
-#         #data = mujoco.MjData(model)
-
-        
-#         #print(data.joint('A1').qpos) # ez működik, de ez a másik mujoco
-        
-# # https://mujoco.readthedocs.io/en/2.1.2/python.html
-
-    
-#     def move(self,y):
-#         print("move")
-#         #self.sim.data.qpos[A1_qpos] = 0.2
-#         #self.sim.data.qpos[A2_qpos] = 0.2
-#         #self.sim.data.qpos[A3_qpos] = 0.2
-#         self.sim.data.qvel[A1_qvel] = 0.5
-#         self.sim.data.qvel[A2_qvel] = 0.5
-#         self.sim.data.qvel[A3_qvel] = 0.5
-
-
-    
-#     def zero(self,y):
-#         print("zero")
-#         #self.sim.data.qpos[A1_qpos] = 0
-#         #self.sim.data.qpos[A2_qpos] = 0
-#         #self.sim.data.qpos[A3_qpos] = 0
-#         self.sim.data.qvel[A1_qvel] = -0.5
-#         self.sim.data.qvel[A2_qvel] = -0.5
-#         self.sim.data.qvel[A3_qvel] = -0.5
-
 
 class EnvClassName(mujoco_env.MujocoEnv, utils.EzPickle):
     def __init__(self):
-    
         utils.EzPickle.__init__(self)
-        FILE_PATH = '' # Absolute path to your .xml MuJoCo scene file 
-                        # OR.
         FILE_PATH = os.path.join(os.path.dirname(__file__), "compiled.xml")
-        frame_skip = 2
+        frame_skip = 1
         mujoco_env.MujocoEnv.__init__(self, FILE_PATH, frame_skip)
-
-
         
     def reset(self):
         self.sim.reset()
@@ -91,123 +23,26 @@ class EnvClassName(mujoco_env.MujocoEnv, utils.EzPickle):
         return np.concatenate([qpos, qvel])
         
     def step(self, action):
-        self.sim.data.ctrl[:] = action
+        # a1 = self.sim.model.get_joint_qpos_addr('A1')
+        # a2 = self.sim.model.get_joint_qpos_addr('A2')
+        # a3 = self.sim.model.get_joint_qpos_addr('A3')
+        # sim_state = self.sim.get_state()
+        # print(action[0])
+        # if action[0]>500:
+        #     sim_state.qpos[a1]=.4
+        #     self.sim.set_state(sim_state)
+        # else:
+        #     sim_state.qpos[a1]=0
+        #     self.sim.set_state(sim_state)
+
+        print(self.sim.model.get_joint_qpos_addr('A1'))
+        
         self.sim.step()
         return self.get_observation(), 0, False, {}
     
-    def _create_action_space(self):
-        action_dim = self.model.nu
-        action_high = np.ones(action_dim)
-        return spaces.Box(-action_high, action_high, dtype=np.float32)
-
-    def _create_observation_space(self):
-        observation_dim = self.model.nq + self.model.nv
-        observation_high = np.inf * np.ones(observation_dim)
-        return spaces.Box(-observation_high, observation_high, dtype=np.float32)
-
-
-
-#######################################################################################################################################
-
-
-# import numpy as np
-# import os
-# from gym import utils, error, spaces
-# from gym.envs.mujoco import mujoco_env
-# from mujoco_py import MjViewer, functions
-
-
-# cunt=0
-# class EnvClassName(mujoco_env.MujocoEnv, utils.EzPickle):
-#     def __init__(self):
-#         utils.EzPickle.__init__(self)
-#         FILE_PATH = '' # Absolute path to your .xml MuJoCo scene file 
-#                         # OR.
-#         FILE_PATH = os.path.join(os.path.dirname(__file__), "compiled.xml")
-#         frame_skip = 5
-#         mujoco_env.MujocoEnv.__init__(self, FILE_PATH, frame_skip)
-#         self.A1 = 3
-#         self.A2 = 17
-#         self.A3 = 31
-
-
-
-#     # self.sim.data.qpos[0:3] - labda
-#     # self.sim.data.qpos[3] - A1 aktuátor
-#     # self.sim.data.qpos[17] - A2
-#     # self.sim.data.qpos[31] - A2
-#     # self.sim.data.qpos[4] - teteje talán vagy az A1 platformnál lévő középrésze VAGY a munkaháromszög?
-#     # self.sim.data.qpos[5] - valami káosz nem tom mi lehet 
-#     def setter(self):
-#         global cunt
-#         cunt+=1
-#         print("QPOS!!!!!!!!!!!!!!!!!!!!!!")
-#         b = self.sim.data.qpos[3]
-#         i=0
-#         self.sim.data.qpos[0] = 0.4
-#         self.sim.data.qpos[1] = 0.4
-#         self.sim.data.qpos[2] = 0.4
-#         if cunt<100:
-#             self.sim.data.qpos[self.A1] = 0.4
-#             self.sim.data.qpos[self.A2] = 0.4
-#             self.sim.data.qpos[self.A3] = 0.4
-#         if cunt>200:
-#             self.sim.data.qpos[self.A1] = 0
-#             self.sim.data.qpos[self.A1] = 0
-#             self.sim.data.qpos[self.A1] = 0
-#             if cunt==300:
-#                 cunt=0
-#         self.sim.data.ctrl[:]
-        
-
-
-#     def step(self, a):
-#         # Carry out one step 
-#         # Don't forget to do self.do_simulation(a, self.frame_skip)
-#         xposbefore = self.get_body_com("base_link")[0]
-#         self.do_simulation(a, self.frame_skip)
-#         # xposafter = self.get_body_com("base_link")[0]
-#         # forward_reward = (xposafter - xposbefore) / self.dt
-#         # ctrl_cost = 0.5 * np.square(a).sum()
-#         # contact_cost = (
-#         #     0.5 * 1e-3 * np.sum(np.square(np.clip(self.sim.data.cfrc_ext, -1, 1)))
-#         # )
-#         # survive_reward = 1.0
-#         # reward = forward_reward - ctrl_cost - contact_cost + survive_reward
-#         # state = self.state_vector()
-#         # notdone = np.isfinite(state).all() and state[2] >= 0.2 and state[2] <= 1.0
-#         # done = not notdone
-#         ob = self._get_obs()
-#         reward = 0
-#         done = False
-#         return (
-#             ob,
-#             reward,
-#             done,
-#             # dict(
-#             #     reward_forward=forward_reward,
-#             #     reward_ctrl=-ctrl_cost,
-#             #     reward_contact=-contact_cost,
-#             #     reward_survive=survive_reward,
-#             # ),
-#             1,
-#         )
-#     def _get_obs(self):
-#         return np.concatenate(
-#             [
-#                 self.sim.data.qpos.flat[2:],
-#                 self.sim.data.qvel.flat,
-#                 np.clip(self.sim.data.cfrc_ext, -1, 1).flat,
-#             ]
-#         )
-#     def reset_model(self):
-#         print("reset")
-#         input()
-#         #qpos = self.init_qpos + self.np_random.uniform(
-#         #    size=self.model.nq, low=-0.1, high=0.1
-#         #)
-#         #qvel = self.init_qvel + self.np_random.standard_normal(self.model.nv) * 0.1
-#         #self.set_state(qpos, qvel)
-#         return self._get_obs()
-#     def viewer_setup(self):
-#         self.viewer.cam.distance = self.model.stat.extent * 0.5
+    def step_sinus(self, action):
+        # This line is responsible to control with SINUS
+        self.sim.data.ctrl[:] = action
+        self.sim.step()
+        return self.get_observation()
+    
--- gtpr/main.py
+++ gtpr/main.py
@@ -3,93 +3,48 @@ import gym
 import FolderB
 import numpy as np
 import os
-
-env = gym.make('GTPR-v0')
-# env.qposvel_zeroing()
-# env.ball_init()
-asd = np.sin(range(10000))
-obs = env.reset()
-while(True):
-    for t in range(1000):
-        #action = env.action_space.sample()
-        action = [asd[t],asd[t],asd[t]]
-        obs, reward, done, info = env.step(action)
-        env.render()
-        if done:
-            print("Episode finished after {} timesteps".format(t+1))
-            obs = env.reset()
-            break
-env.close()
+import math
+import sys
 
 
+def perform_sinus(env, obs):
+    '''
+    This is only perform sinus presentation
+    '''
+    amplitude = 1.2
+    frequency = 20.0
+    time = np.arange(0, 10000, 0.001)
+    sinus = amplitude * np.sin(2 * np.pi * frequency * time)
+
+    while(True):
+        for t in range(10000):
+            action = [sinus[t],sinus[t],sinus[t]]
+            
+            obs = env.step_sinus(action)
+            env.render()
+
+def train(env, obs):
+    while(True):
+        for t in range(10000):
+            action = env.action_space.sample()
+            #action = [sinus[t],sinus[t],sinus[t]]
+            
+            obs, reward, done, info = env.step(action)
+            env.render()
+            if done:
+                print("Episode finished after {} timesteps".format(t+1))
+                obs = env.reset()
+                break
+
+def main():
+    env = gym.make('GTPR-v0')
+    obs = env.reset()
+    if sys.argv[1] == "train":
+        train(env, obs)
+    elif sys.argv[1] == "sin":
+        perform_sinus(env, obs)
+    env.close()
 
 
-# from cmath import sin
-# import gym
-# import FolderB
-# import numpy as np
-
-# env = gym.make('GTPR-v0')
-# #obs = env.reset()
-# # env.qposvel_zeroing()
-# # env.ball_init()
-# asd = np.sin(range(10000))
-# # action = [asd[t],asd[t],asd[t]]
-# while(True):
-#     #obs = env.reset()
-#     for t in range(1000):
-#         action = env.action_space.sample()
-#         # action = [asd[t],asd[t],asd[t]]
-#         obs, reward, done, info = env.step(action)
-#         env.render()
-#         #if done:
-#         #    print("Episode finished after {} timesteps".format(t+1))
-#         #    break
-# env.close()
-
-
-# import gym
-# import FolderB
-
-# env = gym.make('GTPR-v0')
-# obs = env.reset()
-# while(True):
-#     env.ball_init()
-#     for t in range(10000000):
-#         action = env.action_space.sample()
-#         print("action: ",action)
-#         observation = env.observation_space.sample()
-#         print("observation: ",observation)
-#         obs, reward, done, info = env.step(action)
-#         env.render()
-#         env.setter() #invertedPendulum-v2
-#         if done:
-#             print("Episode finished after {} timesteps".format(t+1))
-#             break
-# env.close()
-
-
-
-
-# import gym
-# from network import DQN
-
-# import FolderB
-# print("Sajat kornyezet inditasa!")
-# #env = gym.make('GTPR-v0')
-# #env= lambda obs:0
-# env = gym.make('InvertedPendulum-v2')
-# while(True):
-#     obs = env.reset()
-#     for t in range(10000000):
-#         #env.setter() #invertedPendulum-v2
-#         action = env.action_space.sample()
-#         obs, reward, done, info = env.step(action)
-#         env.render()
-#         lfsz=DQN(env.action_space,128)
-#         print(lfsz.build_model())
-#         input()
-#         if done:
-#             print("Episode finished after {} timesteps".format(t+1))
-#             break
-# env.close()
+if __name__ == "__main__":
+    main()
