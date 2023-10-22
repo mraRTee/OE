ad0776b91370b6541531d97679a819e2a66e6173
mraRTee <tomderoza@gmail.com>
2023-10-22 19:33:41 +0200

    New main.py and reward function, updated xml

--- gtpr/FolderB/envs/MyRobotEnv.py
+++ gtpr/FolderB/envs/MyRobotEnv.py
@@ -20,27 +20,44 @@ class EnvClassName(mujoco_env.MujocoEnv, utils.EzPickle):
     counter_list = []
     distance = []
     reward_list = []
+    prev_deck = 0
     def __init__(self):
         utils.EzPickle.__init__(self)
         FILE_PATH = os.path.join(os.path.dirname(__file__), "compiled.xml")
+        self._max_episode_steps = 1000
         frame_skip = 1
         mujoco_env.MujocoEnv.__init__(self, FILE_PATH, frame_skip)
         
         
     def reset(self):
-        self.sim.reset()
+        # self.sim.reset()
         print("COUNTER: ", self.decking_counter)
         self.decking_counter = 0
         self.biggest = 0
         self.biggest_numbers_of_events = []
         self.elapsed_time = time.time()
         self.reward = 0
+        self.prev_deck = 0
+        
+        
+        qpos = self.init_qpos + self.np_random.uniform(
+            size=self.model.nq, low=-0.01, high=0.01
+        )
+        qvel = self.init_qvel + self.np_random.uniform(
+            size=self.model.nv, low=-0.01, high=0.01
+        )
+        self.set_state(qpos, qvel)
         return self.get_observation()
         
     def get_observation(self):
-        qpos = self.sim.data.qpos
-        qvel = self.sim.data.qvel
-        return np.concatenate([qpos, qvel])
+        # EZEK AZ AKTUÁTOROKHOZ TARTOZÓ JOINT-OK POZÍVIÓJA ÉS SEBESSÉGE
+        # print(self.sim.data.qpos[7]) # A1 qpos
+        # print(self.sim.data.qpos[17]) # A2 qpos
+        # print(self.sim.data.qpos[27]) # A3 qpos)
+        # print(self.sim.data.qvel[6]) # A1 qvel
+        # print(self.sim.data.qvel[16]) # A2 qvel
+        # print(self.sim.data.qvel[27]) # A3 qvel
+        return np.concatenate([self.sim.data.qpos, self.sim.data.qvel]).ravel()
     
     def dimension_getter(self, dimension):
         if dimension == "x":
@@ -73,87 +90,84 @@ class EnvClassName(mujoco_env.MujocoEnv, utils.EzPickle):
         return True if body_1 > body_2 else False
 
     def step(self, action):
+        self.do_simulation(action, self.frame_skip)
+        observations = self.get_observation()
+        done = False
 
-        self.sim.data.ctrl[:] = action
-        self.falling_flag = False
-        self.previous_dist = 0
-        self.sim.step()
-        self.elapsed_time = time.time()
-        return self.get_observation(), 0, False, {}
+        if self.diff('platform', 'ball', "x", "x"):
+            done = True
+
+        reward = self.reward_function(self.get_obs_my('platform', 2), self.get_obs_my('ball', 2), done)
+        return observations, reward, done, {}
+
     
-    def reward_function(self, platform_height, ball_height):
+    def reward_function(self, platform_height, ball_height, done):
         distance_to_target = abs(platform_height - ball_height)
         diff = self.diff('ball', 'platform', "x", "x")
         elapsed_time_in_reward = time.time()-self.elapsed_time
         self.all_ball_height_list.append(ball_height)
 
-        # Ball going upwards 
+        # Ball going up
         if (self.previous_dist < ball_height) and (self.previous_dist != 0 ) and self.falling_flag == True:
-            print("EMELKEDIK")
+            # print("EMELKEDIK")
             self.falling_flag = False
-            # Decking counting up
-            self.decking_counter += 1
+            # Decking counting up - have to set this treshhold because there are some little movement up and down in the environment
+            if self.distance[-1]>.2:
+                self.decking_counter += 1
 
         # Slow down
         # time.sleep(0.04)
 
         # Ball going down
         if self.previous_dist > ball_height and self.falling_flag == False:
-            print("ESIK")
+            # print("ESIK")
             self.highest_balls_list.append(ball_height)
-            self.counter_list.append(self.counter)
+            # self.counter_list.append(self.counter)
             self.falling_flag = True
             # Counting height
             self.distance.append(abs(platform_height - ball_height))
 
-        if self.decking_counter:
-            self.reward += self.highest_balls_list[-1]** 0.02
-            self.reward += self.decking_counter ** 0.2
-            self.reward += elapsed_time_in_reward ** 0.2
 
+        if self.decking_counter > self.prev_deck:
+            # self.reward += self.decking_counter
+            if self.decking_counter >=2:
+                print('WOOOOW: ',self.decking_counter)
+                self.reward += 100*self.decking_counter
+
+        if done is False:
+            if self.distance:
+                self.reward += self.distance[-1]*0.00000001
+            # print(self.reward)
+        else:
+            if self.decking_counter < 2:
+                self.reward -= 1000
+        #     print(self.reward)
+        #     if self.decking_counter:
+        #         self.reward -= .00001/self.decking_counter
+        #     print("a")
+        #     print(self.decking_counter)
+        #     print(self.prev_deck)
+        #     print(done)
+        #     print(self.reward)
+        #     print("b")
         # Getting the latest height of the ball
-        self.counter += 1
+        # self.counter += 1
         self.previous_dist = ball_height
-        return self.reward, self.previous_dist
-
-    def step_train(self, action):
-        done = False
-
-        if self.diff('platform', 'ball', "x", "x"):
-            done = True
-            print("utolsó magas labda: ", self.highest_balls_list[-1])
-            print("dekázás: ", self.decking_counter)
-
-        self.sim.data.ctrl[:] = action
-        self.sim.step()
-        reward, self.previous_dist = self.reward_function(self.get_obs_my('platform', 2), self.get_obs_my('ball', 2))
-
-        return self.get_observation(), reward, done, {}, self.highest_balls_list, self.all_ball_height_list, self.counter_list
-    
-    def q_table_update(self, state_space, action_state):
-
-        # HINT:
-        #  max_future_q = np.max(q_table[new_discrete_state])
-
-        # current_q = q_table[discrete_state + (action,)]
-
-        # new_q = (1 - LEARNING_RATE) * current_q + LEARNING_RATE * (reward + DISCOUNT * max_future_q)
-
-        # q_table[discrete_state + (action,)] = new_q
-        # retrun q_table
-        pass
-
-    def q_table_zeroing(self, state_space, action_state):
-        # state space is amount of elements, action space is the size of elements. 
-        # state_space = 10
-        # action_state = 2
-        q_table = np.zeros((state_space, action_state))
-        print(q_table)
+        self.prev_deck = self.decking_counter
+        return self.reward
 
 
-        # q_table = np.zeros((10000,3))
-        # for i in range(10000):
-        #     for j in range(3):
-        #         q_table[i][j] = np.random.uniform(low=-5, high=0)
+    # def step_train(self, action):
+    #     done = False
 
+    #     if self.diff('platform', 'ball', "x", "x"):
+    #         done = True
+    #         print("utolsó magas labda: ", self.highest_balls_list[-1])
+    #         print("dekázás: ", self.decking_counter)
+
+    #     self.sim.data.ctrl[:] = action
+    #     self.sim.step()
+    #     reward, self.previous_dist = self.reward_function(self.get_obs_my('platform', 2), self.get_obs_my('ball', 2))
+
+    #     return self.get_observation(), reward, done, {}, self.highest_balls_list, self.all_ball_height_list, self.counter_list
   
